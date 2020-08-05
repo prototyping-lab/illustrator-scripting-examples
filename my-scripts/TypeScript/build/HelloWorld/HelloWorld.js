1b98ec6f132b7d61cdfd8f7be002e961b9957873
Martin Schneider <martin.schneider@hfg-gmuend.de>
2020-08-05 18:35:27 +0200

    Add typescript example

---
+++ my-scripts/TypeScript/build/HelloWorld/HelloWorld.js
@@ -0,0 +1,7 @@
+//Hello World generated from Typescript
+var myDocument = app.documents.add();
+//Create a new text frame and assign it to the variable "myTextFrame"
+var myTextFrame = myDocument.textFrames.add();
+// Set the contents and position of the text frame
+myTextFrame.position = [200, 200];
+myTextFrame.contents = "Hello World...";
